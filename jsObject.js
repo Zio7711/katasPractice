3a9bb2040fb92aae52ddd58d111012c95e8f12f3
Zio <zi.ao.tan@outlook.com>
2021-04-19 21:33:48 +0000

    katas practice 17

---
+++ jsObject.js
@@ -0,0 +1,18 @@
+const urlDecode = function(texts) {
+  obj = {}
+  texts = texts.replace(/&/g, ' ')
+  texts = texts.split(' ')
+
+  for(let text of texts) {
+    text = text.replace('=', ' ')
+    text = text.split(' ')
+    let value = text[1].replace(/%20/g, ' ')
+    obj[text[0]] = value
+  }
+  return obj   
+}
+
+console.log(urlDecode("duck=rubber"));
+console.log(urlDecode("bootcamp=Lighthouse%20Labs"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain").weather);
